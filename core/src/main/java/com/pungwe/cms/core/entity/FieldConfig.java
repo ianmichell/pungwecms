60d2c9564c099e5c45d91c02d4e6b02c9a1af4c3
Ian Michell <ianmichell@me.com>
2015-12-01 07:09:20 +0000

    added field type and storage

--- core/src/main/java/com/pungwe/cms/core/entity/FieldConfig.java
+++ core/src/main/java/com/pungwe/cms/core/entity/FieldConfig.java
@@ -1,8 +1,28 @@
 package com.pungwe.cms.core.entity;
 
+import java.util.Map;
+
 /**
  * Created by ian on 01/12/2015.
  */
 public interface FieldConfig {
 
+    int getWeight();
+
+    void setWeight(int weight);
+
+    String getName();
+
+    void setName(String name);
+
+    Map<String, ?> getSettings();
+
+    void setSettings(Map<String, ?> settings);
+
+    Class<FieldType> getFieldType();
+
+    void setFieldType(Class<FieldType> type);
+
+    Class<FieldStorageType> getStorageType();
+    void setStorageType();
 }
---
+++ core/src/main/java/com/pungwe/cms/core/entity/FieldType.java
@@ -0,0 +1,7 @@
+package com.pungwe.cms.core.entity;
+
+/**
+ * Created by ian on 01/12/2015.
+ */
+public interface FieldType {
+}
