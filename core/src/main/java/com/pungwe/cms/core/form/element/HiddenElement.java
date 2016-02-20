d33c5729611bb9768890101e63754209858bf4b3
Ian Michell <ianmichell@me.com>
2016-02-20 10:32:56 +0000

    fixed compilation error with old IntegerElement

---
+++ core/src/main/java/com/pungwe/cms/core/form/element/HiddenElement.java
@@ -0,0 +1,11 @@
+package com.pungwe.cms.core.form.element;
+
+/**
+ * Created by ian on 20/02/2016.
+ */
+public class HiddenElement extends AbstractFormElement<String> {
+	@Override
+	public String getTheme() {
+		return "form/hidden";
+	}
+}
